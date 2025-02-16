d14a9260a343a8461548106d2e249a69d6995cb3
jonzim-cmd <jonzim@posteo.de>
2025-02-16 15:08:52 +0100

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+// src/theme.js
+import { createTheme } from '@mui/material/styles';
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+    background: {
+      default: '#121212',
+      paper: '#1e1e1e',
+    },
+  },
+});
