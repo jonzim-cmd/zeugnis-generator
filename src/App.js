ba43f191e7d151fd9e35bcf76d423ee6438a4fab
jonzim-cmd <jonzim@posteo.de>
2025-02-16 15:22:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Dashboard from './components/Dashboard';
 import { AppProvider } from './context/AppContext';
 import { ThemeProvider } from '@mui/material/styles';
 import { darkTheme } from './theme';
+import { CssBaseline } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <AppProvider>
       <ThemeProvider theme={darkTheme}>
+        {/* CssBaseline setzt die globalen Styles für Dark Mode */}
+        <CssBaseline />
         <Dashboard />
       </ThemeProvider>
     </AppProvider>
