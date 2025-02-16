344f5d794bd8adfa62889436e462242a68d9708e
jonzim-cmd <jonzim@posteo.de>
2025-02-16 15:09:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import { AppProvider } from './context/AppContext';
+import { ThemeProvider } from '@mui/material/styles';
+import { darkTheme } from './theme';
 import './App.css';
 
 function App() {
   return (
     <AppProvider>
-      <Dashboard />
+      <ThemeProvider theme={darkTheme}>
+        <Dashboard />
+      </ThemeProvider>
     </AppProvider>
   );
 }
